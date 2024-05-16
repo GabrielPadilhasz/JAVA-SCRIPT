2a0e04fb5d86babf4bcfef345ed03567bc7ce1ec
GabrielPadilhasz <167728076+GabrielPadilhasz@users.noreply.github.com>
2024-05-16 19:35:47 -0300

    Add files via upload

---
+++ TRABALHO JAVA/script.js
@@ -0,0 +1,18 @@
+
+let calculaSoma = document.querySelector("#calculaSoma");
+let h1titulo = document.querySelector("#h1titulo");
+let PNumero = document.querySelector("#PNumero");
+let SNumero = document.querySelector("#SNumero");
+
+function resultado(){
+    let var1;
+    let var2;
+    var1=Number(PNumero.value);
+    var2=Number(SNumero.value);
+
+    h1titulo.textContent=var1+var2;
+
+}
+calculaSoma.onclick = function(){
+    resultado()
+}
